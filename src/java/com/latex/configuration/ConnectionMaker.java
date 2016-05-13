7f3b91795546e165540c2b6f576dd487146b6b40
Lokendra Prajapati <Lokendra2500@users.noreply.github.com>
2016-05-13 10:15:06 +0530

    Update ConnectionMaker.java

--- src/java/com/latex/configuration/ConnectionMaker.java
+++ src/java/com/latex/configuration/ConnectionMaker.java
@@ -8,7 +8,7 @@ import java.sql.*;
 public class ConnectionMaker {
     
     private static final String user_name = "root";
-    private static final String user_password = "lokendra";
+    private static final String user_password = "password";
     private static final String db_connection = "jdbc:mysql://localhost:3306/latex_database";
     private static final String db_driver = "com.mysql.jdbc.Driver";
     
